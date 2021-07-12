f751705f272d906d1d6cea11088497bd98f5029b
19wh1a0570 <69663938+19wh1a0570@users.noreply.github.com>
2021-07-12 10:06:06 +0530

    Create file1.c

---
+++ CSE/Operating Systems Lab/II CSE-B/19WH1A0570/file1.c
@@ -0,0 +1,16 @@
+#include <sys/stat.h>/* defines S_IREAD & S_IWRITE       */
+int main()
+{
+int fd;
+fd = creat("datafile.dat", S_IREAD | S_IWRITE);
+if (fd == -1)
+printf("Error in opening datafile.dat\n");
+else
+{
+printf("datafile.dat opened for read/write access\n");
+printf("datafile.dat is currently empty\n");
+}
+close(fd);
+exit (0);
+}
+

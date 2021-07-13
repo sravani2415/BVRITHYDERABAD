bce8834190859452c6ec076746745ff4faef9870
19wh1a0590 <69903989+19wh1a0590@users.noreply.github.com>
2021-07-13 12:20:03 +0530

    Delete CSE/JP LAB/II CSE B/19WH1A0590 directory

--- CSE/JP LAB/II CSE B/19WH1A0590/DisplayApplet.java
+++
@@ -1,9 +0,0 @@
-import java.applet.*;
-import java.awt.*;
-public class DisplayApplet extends Applet{
-    public void paint(Graphics g){
-			setForeground(Color.blue);
-			setBackground(Color.pink);
-			g.drawString("Welcome to applet", 50, 60);
-		}
-	}

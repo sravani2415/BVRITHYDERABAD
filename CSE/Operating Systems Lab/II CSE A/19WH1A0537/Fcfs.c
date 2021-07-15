84b6ef4d1c08e97d17471ff27baa55b4d63ae832
19WH1A0537 <86725042+19WH1A0537@users.noreply.github.com>
2021-07-16 01:48:48 +0530

    Add files via upload

---
+++ CSE/Operating Systems Lab/II CSE A/19WH1A0537/Fcfs.c
@@ -0,0 +1,61 @@
+/*FCFS AlGORITHM WITHOUT ARRIVAL TIME*/
+#include<stdio.h>
+void waiting_time(int processes[],int n,int bt[],int wt[]){
+
+wt[0] = 0;
+
+for(int i=1;i<n;i++){
+
+    wt[i] = bt[i-1]+ wt[i-1];
+}
+}
+
+void turn_around_time(int processes[],int n,int bt[],int wt[],int tat[]){
+
+for(int i=0;i<n;i++){
+
+    tat[i] = bt[i]+wt[i];
+}
+}
+
+void average_time(int processes[],int n,int bt[]){
+
+int wt[n],tat[n],total_wt = 0,total_tat = 0;
+
+waiting_time(processes,n,bt,wt);
+turn_around_time(processes,n,bt,wt,tat);
+
+printf("Processes   Burst_Time  Waiting_Time    Turn around_Time\n");
+
+for(int i=0;i<n;i++){
+total_wt += wt[i];
+total_tat += tat[i];
+printf("       %d",processes[i]);
+printf("     \t%d",bt[i]);
+printf("     \t\t%d",wt[i]);
+printf("    \t\t%d\n",tat[i]);
+}
+float s = (float) total_wt / (float) n;
+float t = (float) total_tat / (float) n;
+printf("\nAverage Waiting Time : %0.2f",s);
+printf("\nAverage Turn around Time : %0.2f",t);
+}
+
+int main(){
+
+int num , i;
+printf("Enter the number of Processes : \n");
+scanf("%d",&num);
+int processes[num],burst_time[num];
+int n = sizeof(processes) / sizeof(processes[0]);
+printf("Enter the order of processes in terms of 1,2,3,.....n : \n");
+for(i=0;i<num;i++){
+    scanf("%d",&processes[i]);
+}
+for(i=0;i<num;i++){
+    printf("Enter the burst time of the process %d : ",processes[i]);
+    scanf("%d",&burst_time[i]);
+}
+average_time(processes,n,burst_time);
+return 0;
+}

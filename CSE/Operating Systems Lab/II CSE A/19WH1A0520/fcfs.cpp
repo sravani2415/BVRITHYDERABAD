4219be9b93cd7de46da493054decb12313c3fc7f
19WH1A0520 <86727930+19WH1A0520@users.noreply.github.com>
2021-07-15 16:19:10 +0530

    Add files via upload

---
+++ CSE/Operating Systems Lab/II CSE A/19WH1A0520/fcfs.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+ int bt[20],p[20],wt[20],tat[20],i,j,n,total=0,pos,temp;
+ float avg_wt,avg_tat;
+ printf("Enter number of process:");
+ scanf("%d",&n);
+ 
+ int processes[n];
+ printf("Enter order of processes in the format 1,2,3,4,...,n:");
+ for(i =0;i<n;i++)
+ scanf("%d",&processes[i]);
+ printf("\nEnter Burst Time:\n");
+ for(i=0;i<n;i++)
+ {
+ printf("p % d:",processes[i]);
+ scanf("%d",&bt[i]);
+ p[i] = processes[i];
+ }
+wt[0]=0; 
+ for(i=1;i<n;i++)
+ {
+ wt[i]=0;
+ for(j=0;j<i;j++)
+ wt[i]+=bt[j];
+ total+=wt[i];
+ }
+ avg_wt=(float)total/n;
+ total=0;
+ printf("\nProcess\t Burst Time \tWaiting Time   \tTurnaround Time");
+for(i=0;i<n;i++)
+ {
+ tat[i]=bt[i]+wt[i]; 
+ total+=tat[i];
+ printf("\np%d\t\t %d\t\t %d\t\t \t%d",p[i],bt[i],wt[i],tat[i]);
+ }
+ avg_tat=(float)total/n; 
+ printf("\n\nAverage Waiting Time=%f",avg_wt);
+ printf("\nAverage Turnaround Time=%f\n",avg_tat);
+}

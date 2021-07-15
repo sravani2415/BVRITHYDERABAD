c8113407fd1d01caa07f587f259b225306c61122
19wh1a0557 <86723288+19wh1a0557@users.noreply.github.com>
2021-07-15 15:59:11 +0530

    Add files via upload

---
+++ CSE/Operating Systems Lab/II CSE A/19WH1A0557/fcfs with arrival time.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h> 
+void bubble_sort(int [], int [], int [], int); 
+void swap(int*, int*); 
+int main(){ 
+    int n_proc; 
+	printf("Enter the number of processes: "); 
+	scanf("%d", &n_proc); 
+	int proc[n_proc], burst_time[n_proc], arrival_time[n_proc], waiting_time[n_proc] = {0}, turnaround_time[n_proc]; 
+	printf("\nEnter the order of processes in terms of 1,2,3,4,...n:"); 
+	for(int i = 0; i < n_proc; i++){ 
+	    scanf("%d", &proc[i]); } 
+	for(int i = 0; i < n_proc; i++){ 
+	    printf("Enter the arrival time and burst time of the process p(%d): ", proc[i]); 
+		scanf("%d %d", &arrival_time[i], &burst_time[i]); } 
+	bubble_sort(arrival_time, burst_time, proc, n_proc); 
+	for(int i = 0; i < n_proc; i++){ 
+	    waiting_time[i + 1] += (burst_time[i] + waiting_time[i]); 
+		waiting_time[i] -= arrival_time[i]; } arrival_time[0] = 0; 
+	for(int i = 0; i < n_proc; i++){ 
+		turnaround_time[i] = (waiting_time[i] + burst_time[i]); } 
+	float wait_sum = 0, turn_sum = 0; 
+	printf("Process no.\tArrival time\tBurst time\tWaiting time\tTurnaround time\n"); 
+	for(int i = 0; i < n_proc; i++){ wait_sum += waiting_time[i]; 
+	turn_sum += turnaround_time[i]; 
+	printf("\tp(%d)\t\t%d\t\t%d\t\t%d\t\t%d\n", proc[i], arrival_time[i],burst_time[i], waiting_time[i], turnaround_time[i]); } 
+	printf("Average Waiting time = %.2f ms\n", wait_sum/n_proc); 
+	printf("Average Turn around time = %.2f ms\n",turn_sum/n_proc); 
+	return 0; } 
+	void bubble_sort(int at[], int bt[], int p[], int n){ 
+	    for(int i = 0; i < n; i++){ 
+		    for(int j = 0; j < n - i - 1; j++){ 
+			    if(at[j] > at[j + 1]){ 
+				    swap(&at[j], &at[j + 1]); 
+					swap(&bt[j], &bt[j + 1]); 
+					swap(&p[j], &p[j + 1]); } } } } 
+	void swap(int *a, int *b){ 
+	    int temp = *b; 
+		*b = *a; 
+		*a = temp; }
---
+++ CSE/Operating Systems Lab/II CSE A/19WH1A0557/srtf.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main(){
+    int at[10],bt[10],temp[10],wt1[10],tat1[10],p[20];
+    int i,small,count=0,time,n;
+    int wt = 0,tt = 0,end;
+
+    printf("\nEnter the Total Number of Processes:");
+    scanf("%d", &n);
+    printf("\nEnter Arrival Time and Burst Time:");
+    for(i = 0; i < n; i++)
+    {
+        printf("p%d:",i+1);        
+        scanf("%d%d", &at[i],&bt[i]); 
+        p[i]=i+1;  
+        temp[i] = bt[i];
+    }
+    bt[9] = 9999;
+    for(time = 0; count != n; time++){
+        small = 9;
+        for(i = 0;i<n; i++){
+            if(at[i] <= time && bt[i] < bt[small] && bt[i] > 0)
+                small = i;
+        }
+        bt[small]--;
+        if(bt[small] == 0){
+            count++;
+            end = time + 1;
+            wt = wt + end - at[small] - temp[small];
+            tt = tt + end - at[small];
+            wt1[small] = end - at[small] - temp[small];
+            tat1[small] = end -at[small];
+        }
+    }
+    printf("\nprocess\t    burst_time\t    \twaiting_time      \tturnaround_time\n");
+    for(int i = 0;i<n;i++){
+    	printf("\nP[%d]\t\t  %d\t\t    %d\t\t\t%d",i,temp[i],wt1[i],tat1[i]);
+    }
+    printf("\nAverage Waiting Time: %d\n", wt/n);
+    printf("Average Turnaround Time: %d\n", tt/n);
+      
+    return 0;
+}

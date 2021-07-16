50ad34ffa3c36ab0969cc0177e04c561798a4edb
19wh1a0553 <81098373+19wh1a0553@users.noreply.github.com>
2021-07-16 11:10:19 +0530

    Add files via upload

---
+++ CSE/Operating Systems Lab/II CSE A/19WH1A0553/Bankers_algorithm.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+int main() {
+    int m, n , i , j, k, p;
+    printf("Enter number of resources: ");
+    scanf("%d", &m);
+
+    int available[m];
+    printf("Enter total allocation of resources: ");
+
+    for (i = 0; i < m; i++)
+        scanf("%d", &available[i]);
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    int max[n][m];
+    int allocation[n][m];
+    int need[n][m];
+
+    printf("Enter the number of resources allocated by each process: ");
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++)
+            scanf("%d", &allocation[i][j]);
+    }
+
+    printf("Enter the max need of each process: ");
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++)
+            scanf("%d", &max[i][j]);
+    }
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            available[j] -= allocation[i][j];
+        }
+    }
+
+    int visited[n];
+
+    for (i = 0; i < n; i++) {
+            visited[i] = 0;
+        for (j = 0; j < m; j++)
+            need[i][j] = max[i][j] - allocation[i][j];
+    }
+
+    int safe_seq[n];
+    int ind = 0;
+
+    for (k = 0; k < n; k++) {
+        for (i = 0; i < n; i++) {
+            if (visited[i] == 0) {
+                int flag = 1;
+                for (j = 0; j < m; j++) {
+                    if (need[i][j] > available[j]) {
+                        flag = 0;
+                        break;
+                    }
+                }
+
+                if (flag) {
+                    safe_seq[ind++] = i + 1;
+                    visited[i] = 1;
+                    
+                    for (j = 0; j < m; j++)
+                        available[j] += allocation[i][j];
+                    printf("\nProcess %d is done,available sources are: ", i+1);
+                    for (p = 0; p < m; p++)
+                        printf("%d ", available[p]);
+                }
+            }
+        }
+    }
+    printf("\nSystem is in safe state and the safe sequence is: ");
+    for (i = 0; i < n; i++)
+        printf("%d ", safe_seq[i]);
+}

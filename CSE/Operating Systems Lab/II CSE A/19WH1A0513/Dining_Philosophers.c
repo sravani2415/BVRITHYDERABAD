d3c96b7a89981f9040804559902749e3b4867ec8
19WH1A0513 <86731635+19WH1A0513@users.noreply.github.com>
2021-07-16 14:59:30 +0530

    Add files via upload

---
+++ CSE/Operating Systems Lab/II CSE A/19WH1A0513/Dining_Philosophers.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void wait(int *s){
+	while(*s<=0);
+	*s--;
+}
+void signal(int *s){
+	*s++;
+}
+void dining(int n){
+	int chopstick[n],i;
+	for(i=0;i<n;i++)
+		chopstick[i] = 1;
+	i=0;
+	do{
+		wait(&chopstick[i]);
+		wait(&chopstick[(i+1)%n]);
+		printf("Process %d Eating\n", i+1);
+		signal(&chopstick[i]);
+		signal(&chopstick[(i+1)%n]);
+		printf("Process %d Thinking\n", i+1);
+		i = ((i+1)%n);
+	}while(i);
+}
+int main(){
+	int n,i;
+	printf("Enter number of processes:     ");
+	scanf("%d", &n);
+	dining(n);
+}

793c350d1dc792252665e6bffd00d5975fa8c6f1
19wh1a0525 <87298925+19wh1a0525@users.noreply.github.com>
2021-07-26 20:33:13 +0530

    Add files via upload

---
+++ CSE/JP LAB/II CSE A/19WH1A0525/factorial.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.Applet;
+
+
+public class factorial extends Applet implements ActionListener
+{
+    Button b1; 
+    TextField t1, t2;
+    Label l1, l2;
+
+    public Factorial_3()
+    {
+        l1 = new Label("Num");
+        t1 = new TextField(6);
+
+        b1 = new Button("Compute");
+        
+        l2 = new Label("Factorial");
+        t2 = new TextField(30);
+
+        l1.setBounds(20, 50, 70, 20);
+		t1.setBounds(100, 50, 100, 20);
+		l2.setBounds(20, 90, 70, 20);
+		t2.setBounds(100, 90, 100, 20);
+       
+        add(l1);
+		add(t1);
+
+		add(b1);
+
+		add(l2);
+		add(t2);
+
+        b1.addActionListener(this);
+
+      
+
+    }
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource()==b1)
+        {
+            int fact=fact(Integer.parseInt(t1.getText()));
+            t2.setText(String.valueOf(fact));
+        }
+    }
+    int fact(int f) 
+    {
+        int s=0; 
+        if(f==0)
+            return 1;
+        else
+            return f*fact(f-1);
+    }
+}
